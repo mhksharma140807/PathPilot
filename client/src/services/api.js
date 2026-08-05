9c31d89039975843de3d1d74e6696d386444249a
Mahak Sharma <mhksharma140807@gmail.com>
2026-08-05 17:06:39 +0530

    Fix API base URL for production deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,9 @@ import axios from "axios";
 import { getStoredToken, clearAuthSession } from "../utils/authStorage";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
