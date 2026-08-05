3c684d82d3f8254958339dd5e244c66ae9cca092
Mahak Sharma <mhksharma140807@gmail.com>
2026-08-05 15:29:18 +0530

    fix: add development DNS fallback for MongoDB Atlas

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
+const dns = require('dns')
+if (process.env.NODE_ENV !== "production") {
+  dns.setServers(["8.8.8.8", "8.8.4.4"]);
+}
+
 require("dotenv").config();
 
 const app = require("./app");
 const connectDB = require("./config/db");
+
 
 const PORT = process.env.PORT || 5000;
 
